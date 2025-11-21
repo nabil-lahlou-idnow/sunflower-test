ad859f467aee6cdddc663ac2c60a0523d8c85120
sunflower-developer <sunflower-developer@idnow.io>
2025-11-21 08:57:06 +0100

    Release - Update SDK to version 2.0.0-rc9

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SunflowerUIKit",
-            url: "https://github.com/nabil-lahlou-idnow/sunflower-test/releases/download/2.0.0-rc8/SunflowerUIKit.xcframework.zip",
-            checksum: "22f3c0440eb777727281f7b52a0f8ef9cb791b1afed16d0d352b339725b43ef0"
+            url: "https://github.com/nabil-lahlou-idnow/sunflower-test/releases/download/2.0.0-rc9/SunflowerUIKit.xcframework.zip",
+            checksum: "c73bb8702f2dddac5d98c06052d43bb4caeec0bbc3c8ba604a95df3aabb00635"
         ),
         .binaryTarget(
             name: "SunflowerSwiftUI",
-            url: "https://github.com/nabil-lahlou-idnow/sunflower-test/releases/download/2.0.0-rc8/SunflowerSwiftUI.xcframework.zip",
-            checksum: "c50c981afc47d5240ee520355abd19e430c1b0764e06fd5d971827c03a0101e9"
+            url: "https://github.com/nabil-lahlou-idnow/sunflower-test/releases/download/2.0.0-rc9/SunflowerSwiftUI.xcframework.zip",
+            checksum: "0929a185e36211acd009ab9080e7437da5f74afdf23c6bd3606abd2d31ea91c9"
         ),
         .target(
              // Main target which contains both SunflowerUIKit and the lottie dependency. Automatically downloaded when client fetch Sunflower.
@@ -38,7 +38,7 @@ let package = Package(
                 "SunflowerUIKit",
                 .product(name: "Lottie", package: "lottie-spm")
             ],
-            path: "sources"
+            path: "sources-uikit"
         ),
         .target(
              // Main target which contains both SunflowerSwiftUI and the lottie dependency. Automatically downloaded when client fetch Sunflower.
@@ -47,7 +47,7 @@ let package = Package(
                 "SunflowerSwiftUI",
                 .product(name: "Lottie", package: "lottie-spm")
             ],
-            path: "sources"
+            path: "sources-swiftui"
         )
     ]
 )
